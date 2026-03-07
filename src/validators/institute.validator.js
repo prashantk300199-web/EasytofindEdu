ce077c1a4e2647e72a05d95210af410028caa086
Pawan Kumar <ceo.bookmytickets@gmail.com>
2026-03-07 11:30:26 +0530

    course fixed v2

--- src/validators/institute.validator.js
+++ src/validators/institute.validator.js
@@ -100,6 +100,7 @@ export const createInstituteValidator = Joi.object({
   )
 });
 export const createCourseValidator = Joi.object({
+  institute: Joi.string().hex().length(24).required(),
   name: Joi.string().required(),
   description: Joi.string(),
   mode: Joi.string().valid('English', 'Hindi', 'Hinglish').required()
